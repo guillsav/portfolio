7c8cf5e37c3eb42e5d095dba9b7cab9a63bcaccd
guillsav <guillsav@gmail.com>
2019-10-14 16:07:29 -0400

    Refined about me details text

--- src/pages/about/about.components.jsx
+++ src/pages/about/about.components.jsx
@@ -17,15 +17,14 @@ const AboutPage = () => (
             <AboutDetails>
                 <AboutIMG src={aboutIMG} alt="about-image" />
                 <p>
-                    I am a Web Developer residing in the NYC Greater Area, eager
-                    to learn new technologies, and enjoy problem solving. My
-                    background use to be as a Web Designer, where I worked
-                    closely with Developers and found myself enjoying the
+                    I am a Web Developer residing in the NYC Greater Area with a
+                    background in Web Design. When working as a Web Designer, I
+                    worked closely with Developers and found myself enjoying the
                     technical aspect of building products for clients. This
                     experience lead me to learn Web Development and technologies
                     like HTML, CSS, Javascript, React, Nodejs etc... I am
                     looking forward to combine my knowledge in Web Development
-                    and Web Design and help building products that makes a
+                    and Web Design to help build products that makes a
                     difference.
                 </p>
             </AboutDetails>
