bf5b75536705b2b7b0d90687c1ffa94cd1ad563f
guillsav <guillsav@gmail.com>
2020-05-29 00:41:11 -0400

    Fixed minor UI bugs

--- src/components/Footer/Footer.styles.jsx
+++ src/components/Footer/Footer.styles.jsx
@@ -1,132 +1,133 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
 export const FooterContainer = styled.footer`
-    background: #04040c;
-    color: rgb(255, 255, 255, 0.9);
-    width: 100vw;
-    padding: 0;
-    padding-bottom: 5rem;
-    margin: 0;
-`;
+  background: #04040c;
+  color: rgb(255, 255, 255, 0.9);
+  width: 100vw;
+  padding: 0;
+  padding-bottom: 5rem;
+  margin: 0;
+`
 
 export const FooterWrap = styled.div`
-    max-width: 960px;
-    margin: 0 auto;
-    padding: 0;
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0;
 
-    @media (max-width: 1024px) {
-        max-width: 100vw;
-        margin: 0;
-    }
-`;
+  @media (max-width: 1024px) {
+    max-width: 100vw;
+    margin: 0;
+  }
+`
 
 export const GetInTouch = styled.div`
-    width: 100%;
-    display: grid;
-    align-items: center;
-    grid-template-columns: 2.5fr 2fr 2.5fr;
-    grid-gap: 4rem;
-    padding-top: 5rem;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 2.5fr 2fr 2.5fr;
+  grid-gap: 4rem;
+  padding-top: 5rem;
+  align-items: center;
 
-    @media (max-width: 1024px) {
-        grid-template-columns: 1.5fr 2fr 1.5fr;
-    }
+  @media (max-width: 1024px) {
+    grid-template-columns: 1.5fr 2fr 1.5fr;
+  }
 
-    @media (max-width: 600px) {
-        grid-template-columns: 1fr 2fr 1fr;
-        grid-gap: 1rem;
-    }
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr 2fr 1fr;
+    grid-gap: 1rem;
+  }
 
-    @media (max-width: 420px) {
-        grid-template-columns: 4fr;
-        grid-gap: 0;
-    }
-`;
+  @media (max-width: 420px) {
+    grid-template-columns: 4fr;
+    grid-gap: 0;
+  }
+`
 
 export const Links = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding-top: 1.8rem;
-`;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 1.8rem;
+`
 
 export const Divider = styled.div`
-    height: 1px;
-    background: rgb(255, 255, 255, 0.9);
+  height: 1px;
+  background: rgb(255, 255, 255, 0.9);
 
-    @media (max-width: 420px) {
-        display: none;
-    }
-`;
+  @media (max-width: 420px) {
+    display: none;
+  }
+`
 
 export const FooterTitle = styled.h2`
-    font-size: 3.4rem;
-    color: rgb(255, 255, 255, 0.9);
-    font-weight: 700;
-    margin: 0;
-    padding: 0;
-    text-align: center;
+  font-size: 3.4rem;
+  color: rgb(255, 255, 255, 0.9);
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+  text-align: center;
 
-    @media (max-width: 600px) {
-        font-size: 3rem;
-    }
-`;
+  @media (max-width: 600px) {
+    font-size: 3rem;
+  }
+`
 
 export const ContactMe = styled.button`
-    width: 180px;
-    height: 38px;
-    color: #151515;
-    text-transform: uppercase;
-    background: #50e3c2;
-    font-size: 1.2rem;
-    font-weight: 600;
-    border-radius: 2px;
-    text-align: center;
-    display: flex;
-    padding: 0 3.5rem;
-    justify-content: space-between;
-    text-decoration: none;
-    margin-bottom: 1rem;
-    border: none;
-    transition: all 0.1s ease-out;
-    cursor: pointer;
+  width: 180px;
+  height: 38px;
+  color: #151515;
+  text-transform: uppercase;
+  background: #50e3c2;
+  font-size: 1.2rem;
+  font-weight: 600;
+  border-radius: 2px;
+  text-align: center;
+  display: flex;
+  padding: 0 3.5rem;
+  justify-content: space-between;
+  align-items: center;
+  text-decoration: none;
+  margin-bottom: 1rem;
+  border: none;
+  transition: all 0.1s ease-out;
+  cursor: pointer;
 
-    &:hover {
-        background: none;
-        border: 0.5px solid #50e3c2;
-        color: #50e3c2;
-    }
-
-    @media (max-width: 550px) {
-        padding: 0 2.5rem;
-        justify-content: space-between;
-    }
-
-    @media (max-width: 450px) {
-        width: 130px;
-        heigh: auto;
-        padding: 0 1.5rem;
-        font-size: 1.2rem;
-        justify-content: space-between;
-    }
-`;
-export const Link = styled.a`
-    width: 50px;
-    opacity: 0.9;
-    color: #fff;
-    text-transform: uppercase;
+  &:hover {
     background: none;
+    border: 0.5px solid #50e3c2;
+    color: #50e3c2;
+  }
+
+  @media (max-width: 550px) {
+    padding: 0 2.5rem;
+    justify-content: space-between;
+  }
+
+  @media (max-width: 450px) {
+    width: 130px;
+    heigh: auto;
+    padding: 0 1.5rem;
     font-size: 1.2rem;
-    text-decoration: none;
-`;
+    justify-content: space-between;
+  }
+`
+export const Link = styled.a`
+  width: 50px;
+  opacity: 0.9;
+  color: #fff;
+  text-transform: uppercase;
+  background: none;
+  font-size: 1.2rem;
+  text-decoration: none;
+`
 
 export const Socials = styled.div`
-    width: 180px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 2.2rem;
-    align-items: baseline;
-    margin-top: 1rem;
-`;
+  width: 180px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 2.2rem;
+  align-items: baseline;
+  margin-top: 1rem;
+`
--- src/components/Navigation/Navigation.styles.jsx
+++ src/components/Navigation/Navigation.styles.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
 // export const NavBar = styled.nav`
 //     background: #0f0f0f;
@@ -16,54 +16,54 @@ import styled from 'styled-components';
 // `;
 
 export const NavBar = styled.nav`
-    background: #04040c;
-    border-top: 1px solid rgb(255, 255, 255, 0.08);
-    width: 100vw;
-    height: 5.4rem;
-    color: #fff;
-    font-size: 1.4rem;
-    text-transform: uppercase;
-    display: flex;
-    align-items: center;
-    padding: 0;
-    margin: 0;
+  background: #04040c;
+  border-top: 1px solid rgb(255, 255, 255, 0.08);
+  width: 100vw;
+  height: 5.4rem;
+  color: #fff;
+  font-size: 1.4rem;
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  padding: 0;
+  margin: 0;
 
-    @media (max-width: 768px) {
-        font-size: 1.2rem;
-    }
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+  }
 
-    @media (max-width: 320px) {
-        font-size: 1.1rem;
-    }
-`;
+  @media (max-width: 320px) {
+    font-size: 1.1rem;
+  }
+`
 
 export const NavContainer = styled.div`
-    width: 960px;
-    max-width: 960px;
-    margin: 0 auto;
-    display: grid;
-    grid-template-columns: 2fr 3fr;
-    grid-gap: 1.2rem;
-    padding: 0;
-    align-items: center;
-    justify-content: flex-start;
+  width: 960px;
+  max-width: 960px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+  grid-gap: 1.2rem;
+  padding: 0;
+  align-items: center;
+  justify-content: flex-start;
 
-    @media (max-width: 1024px) {
-        grid-gap: 1rem;
-        margin: 0 4%;
-    }
+  @media (max-width: 1024px) {
+    grid-gap: 1rem;
+    margin: 0 4%;
+  }
 
-    @media (max-width: 700px) {
-        width: auto;
-        grid-template-columns: 2fr 2.5fr;
-        margin: 0 4%;
-    }
+  @media (max-width: 700px) {
+    width: auto;
+    grid-template-columns: 2fr 2.5fr;
+    margin: 0 4%;
+  }
 
-    @media (max-width: 500px) {
-        grid-template-columns: 2fr 1.5fr;
-        margin: 0 2%;
-    }
-`;
+  @media (max-width: 500px) {
+    grid-template-columns: 2fr 1.5fr;
+    margin: 0 2%;
+  }
+`
 // `;
 
 // export const NavContainer = styled.div`
@@ -89,76 +89,77 @@ export const NavContainer = styled.div`
 // `;
 
 export const Pages = styled.div`
-    margin: 0;
-    padding: 0;
-    margin-right: 16rem;
+  margin: 0;
+  padding: 0;
+  margin-right: 16rem;
 
-    @media (max-width: 1024px) {
-        margin-right: 6rem;
-    }
+  @media (max-width: 1024px) {
+    margin-right: 6rem;
+  }
 
-    @media (max-width: 750px) {
-        margin-right: 1rem;
-    }
-`;
+  @media (max-width: 750px) {
+    margin-right: 1rem;
+  }
+`
 
-export const CallToAction = styled.div``;
+export const CallToAction = styled.div``
 
 export const GetInTouch = styled.button`
-    width: 180px;
-    height: 38px;
-    background: #50e3c2;
-    color: #040420;
-    border: none;
-    text-transform: uppercase;
-    font-size: 1.2rem;
-    font-weight: 700;
-    border-radius: 2px;
-    border: none;
-    display: flex;
-    padding: 0 3.5rem;
+  width: 180px;
+  height: 38px;
+  background: #50e3c2;
+  color: #040420;
+  border: none;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  font-weight: 700;
+  border-radius: 2px;
+  border: none;
+  display: flex;
+  padding: 0 3.5rem;
+  justify-content: space-between;
+  align-items: center;
+  transition: all 0.1s ease-out;
+  text-decoration: none;
+  cursor: pointer;
+
+  &:hover {
+    background: none;
+    color: #50e3c2;
+    border: 0.5px solid #50e3c2;
+  }
+
+  @media (max-width: 550px) {
+    padding: 0 2.5rem;
     justify-content: space-between;
-    transition: all 0.1s ease-out;
-    text-decoration: none;
-    cursor: pointer;
+  }
 
-    &:hover {
-        background: none;
-        color: #50e3c2;
-        border: 0.5px solid #50e3c2;
-    }
-
-    @media (max-width: 550px) {
-        padding: 0 2.5rem;
-        justify-content: space-between;
-    }
-
-    @media (max-width: 450px) {
-        width: 130px;
-        heigh: auto;
-        padding: 0 1rem;
-        font-size: 1.2rem;
-        justify-content: space-between;
-    }
-`;
+  @media (max-width: 450px) {
+    width: 130px;
+    heigh: auto;
+    padding: 0 1rem;
+    font-size: 1.2rem;
+    justify-content: space-between;
+  }
+`
 
 export const Link = styled.a`
-    border: 0;
-    text-decoration: none;
-    color: #fff;
-    margin-right: 2rem;
-    text-align: left;
-    transition: all 0.2s ease-out;
+  border: 0;
+  text-decoration: none;
+  color: #fff;
+  margin-right: 2rem;
+  text-align: left;
+  transition: all 0.2s ease-out;
 
-    &:hover {
-        opacity: 1;
-    }
+  &:hover {
+    opacity: 1;
+  }
 
-    &.active {
-        opacity: 1;
-    }
+  &.active {
+    opacity: 1;
+  }
 
-    @media (max-width: 500px) {
-        margin-right: 1.2rem;
-    }
-`;
+  @media (max-width: 500px) {
+    margin-right: 1.2rem;
+  }
+`
